1c025ed6072dab4de5c3647648af84cc5ecc069a
Cody Eivers <codyeivers10@gmail.com>
2023-02-03 17:11:46 -0500

    Code changes

--- app/src/main/java/com/example/gamepedia/DatabaseFiles/GameDAO.java
+++ app/src/main/java/com/example/gamepedia/DatabaseFiles/GameDAO.java
@@ -1,7 +1,9 @@
 package com.example.gamepedia.DatabaseFiles;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -16,21 +18,34 @@ import java.util.List;
 public abstract class GameDAO {
     @Query("SELECT * FROM GAMES")
     public abstract List<GameItem> getAllGames();
+    @Query("SELECT * FROM GAMES WHERE id = :id")
+    public abstract GameItem getGameById(String id);
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    public abstract void insertGames(List<GameItem> gameItems);
+
     @Insert
     public abstract void insert(GameItem gameItem);
+
     @Update
     public abstract void update(GameItem gameItem);
 
+
     public void addGameItem(GameItem gameItem) {
+        gameItem.setTimeStamp(System.currentTimeMillis());
         List<GameItem> existingGames = getAllGames();
         for (GameItem existingGame : existingGames) {
             if (existingGame.getId().equals(gameItem.getId())) {
+                gameItem.setTimeStamp(System.currentTimeMillis());
                 update(gameItem);
                 return;
             }
         }
+        gameItem.setTimeStamp(System.currentTimeMillis());
         insert(gameItem);
     }
 
-    public void updateGameItem(GameItem gameItem) { update(gameItem); }
+    public void updateGameItem(GameItem gameItem) {
+        gameItem.setTimeStamp(System.currentTimeMillis());
+        update(gameItem);
+    }
 }
