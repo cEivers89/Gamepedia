f83b864722c574de9bf049e1bbdd898fa749c550
Cody Eivers <codyeivers10@gmail.com>
2023-02-03 17:14:38 -0500

    Added column last_updated column to db. Will be used to track if a game has been updated from the API

--- app/src/main/java/com/example/gamepedia/DatabaseFiles/GameDatabase.java
+++ app/src/main/java/com/example/gamepedia/DatabaseFiles/GameDatabase.java
@@ -2,6 +2,7 @@ package com.example.gamepedia.DatabaseFiles;
 
 
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.gamepedia.GameFiles.GameItem;
@@ -9,7 +10,7 @@ import com.example.gamepedia.GameFiles.GameItem;
 /**
  * Database class using Room library
  * */
-@Database(entities = {GameItem.class}, version = 1, exportSchema = false)
+@Database(entities = {GameItem.class}, version = 2, exportSchema = false)
 public abstract class GameDatabase extends RoomDatabase {
     public abstract GameDAO gameDAO();
 }
