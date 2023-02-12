98e31be88f69c54139fefe1f54b7e40919f7be95
Cody Eivers <codyeivers10@gmail.com>
2023-02-11 20:01:21 -0500

    added main url to Constants class

--- app/src/main/java/com/example/gamepedia/Constants.java
+++ app/src/main/java/com/example/gamepedia/Constants.java
@@ -11,7 +11,9 @@ public class Constants {
     static Random random = new Random();
     static int maxPage = 200;
     public static int page = random.nextInt(maxPage) + 1;
-    public static final String API_KEY = "9a82209ea5e94110a541bc42039a77a1";
+    public static final String API_KEY = "&key=9a82209ea5e94110a541bc42039a77a1";
+
+    public static final String API_URL = "https://api.rawg.io/api/games?";
 
 
     public static void fetchImage(String url, ImageView imageView, Context context) {
