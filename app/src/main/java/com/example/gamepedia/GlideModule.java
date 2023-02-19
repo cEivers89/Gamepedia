f976991d5c3866465264bcfcbe1a02e4614091f0
Cody Eivers <codyeivers10@gmail.com>
2023-02-19 09:45:53 -0500

    Used to generate the 'GeneratedAppGlideModule' class to get rid of Glide warning

---
+++ app/src/main/java/com/example/gamepedia/GlideModule.java
@@ -0,0 +1,8 @@
+package com.example.gamepedia;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+@com.bumptech.glide.annotation.GlideModule
+public class GlideModule extends AppGlideModule {
+    // used to generate the 'GeneratedAppGlideModule'
+}
