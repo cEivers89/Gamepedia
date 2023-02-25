ec39fcfb3f8016afab7341571083059a8f1732fd
Cody Eivers <codyeivers10@gmail.com>
2023-02-25 17:11:38 -0500

    Refactored to use Fragments

--- app/src/main/java/com/example/gamepedia/MainActivity.java
+++ app/src/main/java/com/example/gamepedia/MainActivity.java
@@ -1,59 +1,59 @@
 package com.example.gamepedia;
 
-
-
-
-import static android.content.ContentValues.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.PagerSnapHelper;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.SnapHelper;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.example.gamepedia.GameFiles.GameAdapter;
-import com.example.gamepedia.GameFiles.GameHeaderAdapter;
-
-import com.example.gamepedia.GameFiles.RecentGamesViewModel;
-import com.example.gamepedia.GameFiles.TopGamesViewModel;
+import com.example.gamepedia.Fragments.RecentGamesFragment;
+import com.example.gamepedia.Fragments.TopGamesFragment;
+import com.example.gamepedia.Fragments.UpcomingGamesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView gameRecyclerView;
-    RecyclerView headerRecyclerView;
-    SnapHelper snapHelper = new PagerSnapHelper();
-    private GameAdapter gameAdapter;
-    private GameHeaderAdapter gameHeaderAdapter;
-    private RecentGamesViewModel recentGamesViewModel;
-    private TopGamesViewModel topGamesViewModel;
+    BottomNavigationView bottomNavView;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gameRecyclerView = findViewById(R.id.game_list_body);
-        headerRecyclerView = findViewById(R.id.game_list_header);
-        // Snaps headerRecyclerView into place
-        snapHelper.attachToRecyclerView(headerRecyclerView);
+        bottomNavView = findViewById(R.id.bottom_nav_view);
 
-        // Sets recent games in the body
-        recentGamesViewModel = new ViewModelProvider(this).get(RecentGamesViewModel.class);
-        recentGamesViewModel.getGameItemsLiveData().observe(this, gameItems ->  {
-            gameAdapter = new GameAdapter(MainActivity.this, gameItems);
-            gameRecyclerView.setAdapter(gameAdapter);
-            gameRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false));
-        });
+        // Retrieve top and recent game fragments
+        Fragment topGamesFragment = new TopGamesFragment();
+        Fragment recentGamesFragment = new RecentGamesFragment();
 
-        // Sets top games to the header
-        topGamesViewModel = new ViewModelProvider(this).get(TopGamesViewModel.class);
-        topGamesViewModel.getTopGamesLiveData().observe(this, headerItems -> {
-            gameHeaderAdapter = new GameHeaderAdapter(MainActivity.this, headerItems);
-            headerRecyclerView.setAdapter(gameHeaderAdapter);
-            headerRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.HORIZONTAL, false));
+        // Set the top and recent games fragments as the default fragments
+        getSupportFragmentManager().beginTransaction().add(R.id.top_games_fragment, topGamesFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.recent_games_fragment, recentGamesFragment).commit();
+
+        // Handles clicks on BottomNavView items here based on item's ID
+        bottomNavView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_home:
+                    Intent home = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(home);
+                    overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
+                    finish();
+                    return true;
+                case R.id.menu_upcoming_games:
+                    // TODO: Upcoming games functionality
+                    // Switch to UpcomingGamesFragment
+                    getSupportFragmentManager().beginTransaction()
+                            .setCustomAnimations(R.anim.slide_up, R.anim.slide_down)
+                            .replace(R.id.recent_games_fragment, new UpcomingGamesFragment())
+                            .commit();
+                case R.id.menu_favorites:
+                    // TODO: Favorites functionality
+                    return true;
+                case R.id.menu_search:
+                    // TODO: Search functionality
+                    return true;
+            }
+            return false;
         });
     }
 
@@ -70,10 +70,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        recentGamesViewModel = null;
-        gameAdapter = null;
-        gameHeaderAdapter = null;
-        snapHelper.attachToRecyclerView(null);
     }
 }
     /* private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
