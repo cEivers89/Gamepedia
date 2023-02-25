371c1b786dac53652422f687decdbc4306f28d13
Cody Eivers <codyeivers10@gmail.com>
2023-02-25 17:04:28 -0500

    Comment added

--- app/src/main/java/com/example/gamepedia/Constants.java
+++ app/src/main/java/com/example/gamepedia/Constants.java
@@ -5,6 +5,9 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+/**
+ * Provides constant variables and/or functions that will never change
+ */
 public class Constants {
     public static final String API_KEY = "&key=9a82209ea5e94110a541bc42039a77a1";
     public static final String API_URL = "https://api.rawg.io/api/games?";
