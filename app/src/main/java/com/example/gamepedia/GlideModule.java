266da260c9e2e74c8ddbdc39922b30ceef8cf6e4
Cody Eivers <codyeivers10@gmail.com>
2023-02-25 17:10:05 -0500

    comment added

--- app/src/main/java/com/example/gamepedia/GlideModule.java
+++ app/src/main/java/com/example/gamepedia/GlideModule.java
@@ -2,7 +2,8 @@ package com.example.gamepedia;
 
 import com.bumptech.glide.module.AppGlideModule;
 
+/**
+ * used to generate 'GeneratedAppGlideModule'
+ * */
 @com.bumptech.glide.annotation.GlideModule
-public class GlideModule extends AppGlideModule {
-    // used to generate the 'GeneratedAppGlideModule'
-}
+public class GlideModule extends AppGlideModule {}
