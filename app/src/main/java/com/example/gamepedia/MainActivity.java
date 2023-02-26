bac3c55bf38d37a454177acd6f674a289dfcde6a
Cody Eivers <codyeivers10@gmail.com>
2023-02-26 15:16:23 -0500

    Refactored fragment navigation

--- app/src/main/java/com/example/gamepedia/MainActivity.java
+++ app/src/main/java/com/example/gamepedia/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.gamepedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
+
 import android.os.Bundle;
-import com.example.gamepedia.Fragments.RecentGamesFragment;
+import android.view.MenuItem;
+
 import com.example.gamepedia.Fragments.TopGamesFragment;
-import com.example.gamepedia.Fragments.UpcomingGamesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView bottomNavView;
 
     @SuppressLint("NonConstantResourceId")
@@ -22,39 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavView = findViewById(R.id.bottom_nav_view);
 
-        // Retrieve top and recent game fragments
+        // Retrieve top game fragment
         Fragment topGamesFragment = new TopGamesFragment();
-        Fragment recentGamesFragment = new RecentGamesFragment();
 
-        // Set the top and recent games fragments as the default fragments
+        // Set the top games fragment as the default fragment
+        // This is so top games isn't reloaded everytime we navigate to a different view
         getSupportFragmentManager().beginTransaction().add(R.id.top_games_fragment, topGamesFragment).commit();
-        getSupportFragmentManager().beginTransaction().add(R.id.recent_games_fragment, recentGamesFragment).commit();
+        // Initiates the NavController
+        initNav();
+    }
 
-        // Handles clicks on BottomNavView items here based on item's ID
-        bottomNavView.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.menu_home:
-                    Intent home = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(home);
-                    overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
-                    finish();
-                    return true;
-                case R.id.menu_upcoming_games:
-                    // TODO: Upcoming games functionality
-                    // Switch to UpcomingGamesFragment
-                    getSupportFragmentManager().beginTransaction()
-                            .setCustomAnimations(R.anim.slide_up, R.anim.slide_down)
-                            .replace(R.id.recent_games_fragment, new UpcomingGamesFragment())
-                            .commit();
-                case R.id.menu_favorites:
-                    // TODO: Favorites functionality
-                    return true;
-                case R.id.menu_search:
-                    // TODO: Search functionality
-                    return true;
-            }
-            return false;
-        });
+    private void initNav() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController =navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavView, navController);
+        bottomNavView.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -70,6 +61,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    // Static Map to map menu item IDs to fragment IDs
+    // Provides more flexible and scalable way to map menu item IDs to fragment IDs
+    private static final Map<Integer, Integer> MENU_ID_TO_FRAGMENT_ID_MAP = new HashMap<Integer, Integer>() {{
+        put(R.id.menu_home, R.id.recent_games_screen);
+        put(R.id.menu_upcoming_games, R.id.upcoming_games_screen);
+        put(R.id.menu_favorites, R.id.favorite_games_screen);
+    }};
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Integer fragmentId = MENU_ID_TO_FRAGMENT_ID_MAP.get(item.getItemId());
+        if (fragmentId != null) {
+            Navigation.findNavController(this, R.id.nav_host_fragment)
+                    .navigate(fragmentId, null, getNavOptions());
+            item.setChecked(true);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private NavOptions getNavOptions() {
+        return new NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_left)
+                .setPopEnterAnim(R.anim.slide_in_left)
+                .setPopExitAnim(R.anim.slide_out_right)
+                .build();
     }
 }
     /* private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
